533ad941b5f2d2a484c2aeccc6c146a5c912d3fe
rakshithaha <116989515+rakshithaha@users.noreply.github.com>
2022-11-01 16:05:36 +0530

    Add files via upload

---
+++ pg12.py
@@ -0,0 +1,6 @@
+n=int(input('Enter a number'))
+sum=0
+for i in range(1,n):
+    sum=sum+i
+    print(sum)
+i=i+1
---
+++ pg8.py
@@ -0,0 +1,7 @@
+num=int(input('Enter the number'))
+if(num==0):
+    print('The entered number is zero')
+elif(num>0):
+    print('The entered number is a positive number')
+else:
+    print('The entered number is a negative number')
